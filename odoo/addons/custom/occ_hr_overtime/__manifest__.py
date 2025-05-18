a842763758543e8456933cc2818739aeba30ad80
NexifAIdev <nexifai.solutions@gmail.com>
2025-05-18 14:08:59 +0000

    Add updated HR Overtime model and related improvements

--- odoo/addons/custom/occ_hr_overtime/__manifest__.py
+++ odoo/addons/custom/occ_hr_overtime/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','ohrms_core'],
+    'depends': ['base','ohrms_core','occ_overtime'],
 
     # always loaded
     'data': [
